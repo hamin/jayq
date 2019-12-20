367f20857a081449bb25215da38b1ff4b2fa05c5
Haris Amin <aminharis7@gmai>
2019-12-19 23:13:47 -0500

    Some cleanup, added verbose flag

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 )
 
 func parse(d string) map[string]interface{} {
-    log.Println(d)
     var v map[string]interface{}
     if err := json.NewDecoder(bytes.NewBufferString(d)).Decode(&v); err != nil {
         log.Fatal(err)
@@ -25,32 +24,13 @@ func parse(d string) map[string]interface{} {
 func main() {
     log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-    // var count int
-    // var conditions string
-    // flag.StringVar(&conditions, "q", "", "query i.e. {}")
-    // flag.Parse()
-    // if flag.Arg(0) == "" {
-    //     log.Fatal("Missing query!")
-    // }
-
     conditions := flag.String("q", "", "query")
+    verbose := flag.Bool("v", false, "verbose")
     flag.Parse()
 
-    log.Println(*conditions)
-
-    // var in io.Reader
-    // if filename := flag.Arg(0); filename != "" {
-    //     f, err := os.Open(filename)
-    //     if err != nil {
-    //         fmt.Println("error opening file: err:", err)
-    //         os.Exit(1)
-    //     }
-    //     defer f.Close()
-
-    //     in = f
-    // } else {
-    //     in = os.Stdin
-    // }
+    if *verbose == true {
+        log.Printf("Conditions: %v\n", *conditions)
+    }
 
     var buf bytes.Buffer
     reader := bio.NewReader(os.Stdin)
@@ -70,12 +50,7 @@ func main() {
 
     }
 
-    // conds := parse(`{
-    //         "id": 1
-    //     }`)
     conds := parse(string(*conditions))
-
-    log.Println("*************")
 
     keyvalueslice := make([]map[string]interface{}, 0)
     err := json.Unmarshal(buf.Bytes(), &keyvalueslice)
@@ -86,14 +61,16 @@ func main() {
 
     enc := json.NewEncoder(os.Stdout)
 
+    numFoundMatches := 0
+    fmt.Println("")
+
     for _, jsonMap := range keyvalueslice {
         if match, err := connor.Match(conds, jsonMap); err != nil {
             log.Fatal("failed to run match:", err)
         } else if match {
-            fmt.Println("Matched")
             enc.Encode(jsonMap)
-        } else {
-            fmt.Println("No Match")
         }
     }
+
+    fmt.Printf("\nFound %d matches", numFoundMatches)
 }
